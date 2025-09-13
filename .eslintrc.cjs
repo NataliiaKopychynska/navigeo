4c10f31bcfb2783619286c9b342fca71bcb39289
Nataliia <nkopichinckaya@gmail.com>
2025-09-13 21:29:01 +0200

    add start file and structure

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  settings: {
+    react: { version: "detect" },
+  },
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off", // бо Vite з React 18 не вимагає import React
+  },
+};
